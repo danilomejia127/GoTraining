d7a5e4523746b5b609bea9b7c385bbddf3236103
Danilo Mejia <danilomejia127@gmail.com>
2022-10-05 21:36:16 -0500

    start morse code

---
+++ text_to_morse/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+ * Reto #9
+ * CÓDIGO MORSE
+ * Fecha publicación enunciado: 02/03/22
+ * Fecha publicación resolución: 07/03/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Crea un programa que sea capaz de transformar texto natural a código morse y viceversa.
+ * - Debe detectar automáticamente de qué tipo se trata y realizar la conversión.
+ * - En morse se soporta raya "—", punto ".", un espacio " " entre letras o símbolos y dos espacios entre palabras "  ".
+ * - El alfabeto morse soportado será el mostrado en https://es.wikipedia.org/wiki/Código_morse.
+ *
+ */
+func main() {
+
+	for k, elem := range getMorseCode() {
+		fmt.Println(k + "=" + elem)
+	}
+
+}
+
+func getMorseCode() map[string]string {
+	var codeMorse = make(map[string]string)
+	codeMorse["A"] = "-."
+	codeMorse["B"] = "-..."
+
+	return codeMorse
+}
