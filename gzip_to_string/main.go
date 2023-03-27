be92aadb0d24fbe4542668510ea5c6ea2df3812b
DaniloMejia <danilomejia127@gmail.com>
2023-03-27 16:56:43 -0500

    add InnerValue

--- gzip_to_string/main.go
+++ gzip_to_string/main.go
@@ -10,6 +10,7 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -25,11 +26,11 @@ func main() {
 
 }
 
-func getKeyInfosFromFile(raw []byte) ([]KeyInfo, error) {
+func getKeyInfosFromFile(raw []byte) ([]WrapperKvsV2, error) {
 	gzreader, err := gzip.NewReader(bytes.NewReader(raw))
 	if err != nil {
 		fmt.Printf("failed gzip.NewReader %v", err)
-		return []KeyInfo{}, err
+		return []WrapperKvsV2{}, err
 	}
 
 	scanner := bufio.NewScanner(gzreader)
@@ -37,36 +38,48 @@ func getKeyInfosFromFile(raw []byte) ([]KeyInfo, error) {
 	scanner.Buffer(buf, 1024*1024)
 	scanner.Split(bufio.ScanLines)
 
-	result := make([]KeyInfo, 0, 10)
+	var result []WrapperKvsV2
 
 	for scanner.Scan() {
 		var item OSItem
 		err := json.Unmarshal([]byte(scanner.Text()), &item)
 		if err != nil {
 			fmt.Printf("failed Unmarshal %v", err)
-			return []KeyInfo{}, err
+			return []WrapperKvsV2{}, err
 		}
 
 		decoded, err := base64.StdEncoding.DecodeString(item.Item.CompressedValue.B)
 		if err != nil {
 			fmt.Printf("failed DecodeString %v", err)
-			return []KeyInfo{}, err
+			return []WrapperKvsV2{}, err
 		}
 
 		decompressed, err := gzip.NewReader(bytes.NewReader(decoded))
 		if err != nil {
 			fmt.Printf("failed gzip.NewReader %v", err)
-			return []KeyInfo{}, err
+			return []WrapperKvsV2{}, err
 		}
 
 		value, err := io.ReadAll(decompressed)
 		if err != nil {
-			return []KeyInfo{}, err
+			return []WrapperKvsV2{}, err
 		}
 
-		keyInfo := KeyInfo{
-			Key:  item.Item.Key.S,
-			Info: string(value),
+		var innerValue InnerValue
+
+		err = json.Unmarshal(value, &innerValue)
+		if err != nil {
+			fmt.Printf("failed json.Unmarshal InnerValue %v", err)
+			return []WrapperKvsV2{}, err
+		}
+
+		keyInfo := WrapperKvsV2{
+			Key:   item.Item.Key.S,
+			Value: innerValue,
+		}
+
+		if keyInfo.Value.IsInStorage {
+			fmt.Printf("In Storage %v\n", keyInfo.Key)
 		}
 
 		result = append(result, keyInfo)
@@ -88,7 +101,14 @@ type OSItem struct {
 	} `json:"Item"`
 }
 
-type KeyInfo struct {
-	Key  string
-	Info string
+type WrapperKvsV2 struct {
+	Key   string
+	Value InnerValue
+}
+
+type InnerValue struct {
+	IsCompressed bool
+	IsInStorage  bool
+	Data         string
+	LastUpdated  time.Time
 }
