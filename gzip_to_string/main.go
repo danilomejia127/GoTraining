e9837b2c32f3c4c511b350e2508f9d5ee96336df
DaniloMejia <danilomejia127@gmail.com>
2023-03-28 16:47:48 -0500

    send data to KVS

--- gzip_to_string/main.go
+++ gzip_to_string/main.go
@@ -8,12 +8,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"strconv"
 	"time"
 )
 
+const (
+	token      = ""
+	tigerToken = "Bearer " + token
+	apiUrl     = "https://testing-synchronizer-v2--payment-methods-read-v2.furyapps.io"
+	resource   = "/pm-core/repository/custom-list"
+)
+
 func main() {
+	start := time.Now()
 	// Open the gzipped file
 	gzippedFile, err := os.ReadFile("gzip_to_string/22wqaueduq3nhhagjvievoevim.json.gz")
 	if err != nil {
@@ -21,9 +31,70 @@ func main() {
 	}
 
 	keyData, err := getKeyInfosFromFile(gzippedFile)
+	if err != nil {
+		fmt.Errorf("no es posible procesar %w", err)
+		return
+	}
+
+	err = sendDataToKvs(keyData, 1000)
+	if err != nil {
+		return
+	}
 
 	fmt.Println("Fin " + strconv.Itoa(len(keyData)))
+	end := time.Now()
+	duration := end.Sub(start)
+	fmt.Println("Tiempo transcurrido:", duration)
+}
 
+func sendDataToKvs(data []WrapperKvsV2, size int) error {
+	for i := 0; i < len(data); i += size {
+		end := i + size
+		if end > len(data) {
+			end = len(data)
+		}
+		subarray := data[i:end]
+
+		fmt.Printf("Enviando registros del %v al %v \n", i, end)
+		err := requestToReadV2(subarray)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func requestToReadV2(data []WrapperKvsV2) error {
+	requestBody, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	u, _ := url.ParseRequestURI(apiUrl)
+	u.Path = resource
+
+	req, err := http.NewRequest(http.MethodPost, u.String(), bytes.NewBuffer(requestBody))
+	req.Header.Add("X-Tiger-Token", tigerToken)
+	if err != nil {
+		return err
+	}
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Errorf("error con body %w \n", err)
+	}
+
+	fmt.Println(resp.Status)
+	fmt.Println(string(bytes))
+
+	return nil
 }
 
 func getKeyInfosFromFile(raw []byte) ([]WrapperKvsV2, error) {
@@ -44,19 +115,19 @@ func getKeyInfosFromFile(raw []byte) ([]WrapperKvsV2, error) {
 		var item OSItem
 		err := json.Unmarshal([]byte(scanner.Text()), &item)
 		if err != nil {
-			fmt.Printf("failed Unmarshal %v", err)
+			fmt.Errorf("failed Unmarshal %w", err)
 			return []WrapperKvsV2{}, err
 		}
 
 		decoded, err := base64.StdEncoding.DecodeString(item.Item.CompressedValue.B)
 		if err != nil {
-			fmt.Printf("failed DecodeString %v", err)
+			fmt.Errorf("failed DecodeString %w", err)
 			return []WrapperKvsV2{}, err
 		}
 
 		decompressed, err := gzip.NewReader(bytes.NewReader(decoded))
 		if err != nil {
-			fmt.Printf("failed gzip.NewReader %v", err)
+			fmt.Errorf("failed gzip.NewReader %w", err)
 			return []WrapperKvsV2{}, err
 		}
 
@@ -69,7 +140,7 @@ func getKeyInfosFromFile(raw []byte) ([]WrapperKvsV2, error) {
 
 		err = json.Unmarshal(value, &innerValue)
 		if err != nil {
-			fmt.Printf("failed json.Unmarshal InnerValue %v", err)
+			fmt.Errorf("failed json.Unmarshal InnerValue %w", err)
 			return []WrapperKvsV2{}, err
 		}
 
@@ -80,12 +151,12 @@ func getKeyInfosFromFile(raw []byte) ([]WrapperKvsV2, error) {
 
 		if keyInfo.Value.IsInStorage {
 			fmt.Printf("In Storage %v\n", keyInfo.Key)
+		} else {
+			result = append(result, keyInfo)
 		}
-
-		result = append(result, keyInfo)
 	}
 
-	fmt.Println("getKeyInfosFromFile succeeded")
+	fmt.Println("getKeyInfosFromFile OK")
 
 	return result, nil
 }
