b3d1b6baed889f5aec73952350ccf8811d370d4d
DaniloMejia <danilo.mejia@mercadolibre.com.co>
2024-02-09 16:43:15 -0500

    get original KVS data

--- compare_pir_vs_pmc/main.go
+++ compare_pir_vs_pmc/main.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	http.HandleFunc("/pir_vs_pmc", handlePostRequest)
+	http.HandleFunc("/validate_last_update", validateLastKVSUpdate)
 
 	// Iniciar el servidor en el puerto 8080
 	fmt.Println("Servidor escuchando en http://localhost:8080")
@@ -26,27 +27,21 @@ func handlePostRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	token := r.Header.Get("X-Tiger-Token")
-	if token == "" {
-		http.Error(w, "Se esperaba un token en la cabecera X-Tiger-Token", http.StatusUnauthorized)
-		return
-	}
-
-	services.SetToken(token)
-
-	body, err := io.ReadAll(r.Body)
+	err := validateToken(r)
 	if err != nil {
-		http.Error(w, "Error al leer el cuerpo del request", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
 	}
 
-	// Verificar que el cuerpo no esté vacío
-	if len(body) == 0 {
-		http.Error(w, "El cuerpo del request no puede estar vacío", http.StatusBadRequest)
+	body, err := validateBody(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
 	// Decodificar el cuerpo del request JSON
 	var inputData services.InputData
+
 	err = json.Unmarshal(body, &inputData)
 	if err != nil {
 		http.Error(w, "Error al decodificar el JSON", http.StatusBadRequest)
@@ -72,4 +67,76 @@ func handlePostRequest(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Error al escribir la respuesta:", err)
 		return
 	}
+}
+
+func validateLastKVSUpdate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Se esperaba un método POST", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := validateToken(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	body, err := validateBody(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Decodificar el cuerpo del request JSON
+	var inputData services.InputSellers
+
+	err = json.Unmarshal(body, &inputData)
+	if err != nil {
+		http.Error(w, "Error al decodificar el JSON", http.StatusBadRequest)
+		return
+	}
+
+	dataResponse := services.ValidateLastUpdateIntoKVS(inputData)
+
+	// Codificar la respuesta JSON
+	response, err := json.Marshal(dataResponse)
+	if err != nil {
+		http.Error(w, "Error al codificar la respuesta JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// Establecer el encabezado de tipo de contenido y enviar la respuesta
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+
+	if _, err := w.Write(response); err != nil {
+		fmt.Println("Error al escribir la respuesta:", err)
+		return
+	}
+
+}
+
+func validateBody(r *http.Request) ([]byte, error) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return []byte{}, fmt.Errorf("error al leer el cuerpo del request")
+	}
+
+	// Verificar que el cuerpo no esté vacío
+	if len(body) == 0 {
+		return []byte{}, fmt.Errorf("el cuerpo del request no puede estar vacío")
+	}
+
+	return body, nil
+}
+
+func validateToken(r *http.Request) error {
+	token := r.Header.Get("X-Tiger-Token")
+	if token == "" {
+		return fmt.Errorf("se esperaba un token en la cabecera X-Tiger-Token")
+	}
+
+	services.SetToken(token)
+
+	return nil
 }
--- compare_pir_vs_pmc/services/service.go
+++ compare_pir_vs_pmc/services/service.go
@@ -16,8 +16,9 @@ const (
 	stagingReaderURL      = "https://production-reader-comp-staging-readv2.melioffice.com"
 	createStagingURL      = "https://production-synchronizer-stgweb--payment-methods-synchronizer.furyapps.io/v2/payment-methods/golden-gate/refresh"
 	createProdAndCloneURL = "https://production-synchronizer-v2--payment-methods-synchronizer.furyapps.io/v2/payment-methods/golden-gate/refresh"
-	deleteClonURL         = "https://production-synchronizer-clon--payment-methods-read-v2.furyapps.io/pm-core/repository/custom/"
-	deleteStagingURL      = "https://production-synchronizer-stg--payment-methods-read-v2.furyapps.io/pm-core/repository/custom/"
+	prodSyncClonReadV2URL = "https://production-synchronizer-clon--payment-methods-read-v2.furyapps.io/pm-core/repository/custom/"
+	prodSyncStgReadV2URL  = "https://production-synchronizer-stg--payment-methods-read-v2.furyapps.io/pm-core/repository/custom/"
+	prodSyncReadV2URL     = "https://production-synchronizer-v2--payment-methods-read-v2.furyapps.io/pm-core/repository/custom/"
 	refreshProdURL        = "https://production-synchronizer-v2--payment-methods-synchronizer.furyapps.io/v2/payment-methods/golden-gate/refresh"
 )
 
@@ -31,18 +32,66 @@ type InputData struct {
 	SellerIDs   []int  `json:"seller_ids"`
 }
 
-type DataComparition struct {
-	SiteID    string
-	SellerID  int
-	InProd    bool
-	InClon    bool
-	InStaging bool
+type InputSellers struct {
+	SellerIDs []int `json:"seller_ids"`
 }
 
 type DataResponse struct {
-	SiteID          string
-	SellerID        int
-	OperationDetail string
+	SiteID          string          `json:"site_id,omitempty"`
+	SellerID        int             `json:"seller_id,omitempty"`
+	DataAnalysis    *DataAnalysis   `json:"data_analysis,omitempty"`
+	ProdData        *SellerAnalysis `json:"prod_data,omitempty"`
+	ClonData        *SellerAnalysis `json:"clon_data,omitempty"`
+	StagingData     *SellerAnalysis `json:"staging_data,omitempty"`
+	OperationDetail string          `json:"operation_detail,omitempty"`
+}
+
+type DataAnalysis struct {
+	PaymentMethods  PaymentMethodsNode  `json:"payment_methods_node,omitempty"`
+	Exclusions      ExclusionsNode      `json:"exclusions_node,omitempty"`
+	Groups          GroupsNode          `json:"groups_node,omitempty"`
+	AmountAllowed   AmountAllowedNode   `json:"amount_allowed_node,omitempty"`
+	OwnPromosByUser OwnPromosByUserNode `json:"own_promos_by_user_node,omitempty"`
+}
+
+type PaymentMethodsNode struct {
+	Prod    int `json:"prod,omitempty"`
+	Clon    int `json:"clon,omitempty"`
+	Staging int `json:"stg,omitempty"`
+}
+
+type ExclusionsNode struct {
+	Prod    int `json:"prod,omitempty"`
+	Clon    int `json:"clon,omitempty"`
+	Staging int `json:"stg,omitempty"`
+}
+
+type GroupsNode struct {
+	Prod    int `json:"prod,omitempty"`
+	Clon    int `json:"clon,omitempty"`
+	Staging int `json:"stg,omitempty"`
+}
+
+type AmountAllowedNode struct {
+	Prod    int `json:"prod,omitempty"`
+	Clon    int `json:"clon,omitempty"`
+	Staging int `json:"stg,omitempty"`
+}
+
+type OwnPromosByUserNode struct {
+	Prod    int `json:"prod,omitempty"`
+	Clon    int `json:"clon,omitempty"`
+	Staging int `json:"stg,omitempty"`
+}
+
+type SellerAnalysis struct {
+	ExistsInKVS     bool   `json:"exists_in_kvs,omitempty"`
+	SellerID        string `json:"seller_id,omitempty"`
+	PaymentMethods  int
+	Exclusions      int
+	Groups          int
+	AmountAllowed   int
+	OwnPromosByUser int
 }
 
 type RefreshMessage struct {
@@ -55,12 +104,26 @@ type RefreshMessage struct {
 }
 
 type DataCustom struct {
-	CollectorID          string `json:"collector_id"`
-	CustomPaymentMethods []any  `json:"custom_payment_methods"`
-	Exclusions           []any  `json:"exclusions"`
-	Groups               any    `json:"groups"`
-	AmountAllowed        []any  `json:"amount_allowed"`
-	OwnPromosByUser      any    `json:"own_promos_by_user"`
+	CollectorID          string        `json:"collector_id"`
+	CustomPaymentMethods []interface{} `json:"custom_payment_methods"`
+	Exclusions           []interface{} `json:"exclusions"`
+	Groups               interface{}   `json:"groups"`
+	AmountAllowed        []interface{} `json:"amount_allowed"`
+	OwnPromosByUser      interface{}   `json:"own_promos_by_user"`
+}
+
+type OriginalDataKVS struct {
+	IsCompressed bool   `json:"IsInStorage"`
+	IsInStorage  bool   `json:"IsInStorage"`
+	Data         string `json:"Data"`
+	LastUpdated  string `json:"LastUpdated"`
+}
+
+type LastUpdatedKVS struct {
+	SellerID           int    `json:"seller_id"`
+	ProdLastUpdated    string `json:"prod_last_updated"`
+	ClonLastUpdated    string `json:"clon_last_updated"`
+	StagingLastUpdated string `json:"stag_last_updated"`
 }
 
 func SetToken(t string) {
@@ -69,17 +132,17 @@ func SetToken(t string) {
 	}
 }
 
-func CompareData(inputData InputData) []DataComparition {
-	results := make([]DataComparition, 0)
+func CompareData(inputData InputData) []DataResponse {
+	results := make([]DataResponse, 0)
 	refreshProdData(inputData)
 
 	for _, seller := range inputData.SellerIDs {
-		sellerResponse := DataComparition{
-			SiteID:    inputData.SiteID,
-			SellerID:  seller,
-			InProd:    getDataCustomFromURL(prodReaderURL, seller),
-			InClon:    getDataCustomFromURL(clonReaderURL, seller),
-			InStaging: getDataCustomFromURL(stagingReaderURL, seller),
+		sellerResponse := DataResponse{
+			SiteID:      inputData.SiteID,
+			SellerID:    seller,
+			ProdData:    getDataCustomFromURL(prodReaderURL, seller),
+			ClonData:    getDataCustomFromURL(clonReaderURL, seller),
+			StagingData: getDataCustomFromURL(stagingReaderURL, seller),
 		}
 
 		results = append(results, sellerResponse)
@@ -96,7 +159,7 @@ func refreshProdData(inputData InputData) {
 	}
 }
 
-func getDataCustomFromURL(url string, seller int) bool {
+func getDataCustomFromURL(url string, seller int) *SellerAnalysis {
 	urlFull := fmt.Sprintf("%s/v2/payment-methods/internal/customizations?collector_id=%d", url, seller)
 
 	client := &http.Client{}
@@ -104,7 +167,7 @@ func getDataCustomFromURL(url string, seller int) bool {
 	req, err := http.NewRequest("GET", urlFull, nil)
 	if err != nil {
 		fmt.Println(err)
-		return false
+		return nil
 	}
 
 	for key, value := range headers {
@@ -114,14 +177,14 @@ func getDataCustomFromURL(url string, seller int) bool {
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
-		return false
+		return nil
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err)
-		return false
+		return nil
 	}
 
 	var dataCustom DataCustom
@@ -129,70 +192,117 @@ func getDataCustomFromURL(url string, seller int) bool {
 	err = json.Unmarshal(body, &dataCustom)
 	if err != nil {
 		fmt.Println(err)
-		return false
+		return nil
 	}
 
-	result, err := isDataInKVS(dataCustom)
+	sellerAnalysis, err := evaluateKVSData(dataCustom)
 	if err != nil {
 		fmt.Println(err)
-		return false
+		return nil
 	}
 
-	return result
+	return &sellerAnalysis
 }
 
-func isDataInKVS(dataCustom DataCustom) (bool, error) {
+func evaluateKVSData(dataCustom DataCustom) (SellerAnalysis, error) {
 	if len(dataCustom.CustomPaymentMethods) == 0 && len(dataCustom.Exclusions) == 0 &&
 		dataCustom.Groups == nil && len(dataCustom.AmountAllowed) == 0 && dataCustom.OwnPromosByUser == nil {
 
-		return false, nil
+		return SellerAnalysis{}, nil
 	}
 
-	return true, nil
+	sellerAnalysis := SellerAnalysis{
+		SellerID:        dataCustom.CollectorID,
+		ExistsInKVS:     true,
+		PaymentMethods:  len(dataCustom.CustomPaymentMethods),
+		Exclusions:      len(dataCustom.Exclusions),
+		Groups:          0,
+		AmountAllowed:   len(dataCustom.AmountAllowed),
+		OwnPromosByUser: 0,
+	}
+
+	if dataCustom.Groups != nil {
+		sellerAnalysis.Groups = len(dataCustom.Groups.([]interface{}))
+	}
+
+	if dataCustom.OwnPromosByUser != nil {
+		sellerAnalysis.OwnPromosByUser = len(dataCustom.OwnPromosByUser.([]interface{}))
+	}
+
+	return sellerAnalysis, nil
 }
 
-func HomologateCustomData(dataComparition []DataComparition) []DataResponse {
+func HomologateCustomData(dataComparison []DataResponse) []DataResponse {
 	dataResponseList := make([]DataResponse, 0)
-	for _, dataSeller := range dataComparition {
+	for _, dataSeller := range dataComparison {
 		dataResponse := DataResponse{
-			SiteID:          dataSeller.SiteID,
-			SellerID:        dataSeller.SellerID,
-			OperationDetail: "Existe en los tres entornos",
+			SiteID:   dataSeller.SiteID,
+			SellerID: dataSeller.SellerID,
 		}
 
-		if dataSeller.InProd == false && dataSeller.InClon == false && dataSeller.InStaging == false {
+		if dataSeller.ProdData.ExistsInKVS == false && dataSeller.ClonData.ExistsInKVS == false && dataSeller.StagingData.ExistsInKVS == false {
 			dataResponse.OperationDetail = "No existe en ningún entorno"
 		}
 
-		if dataSeller.InProd == false && dataSeller.InClon == false && dataSeller.InStaging == true {
-			deletedStaging := deleteDataCustomFromURL(deleteStagingURL, dataSeller.SellerID)
+		if dataSeller.ProdData.ExistsInKVS == false && dataSeller.ClonData.ExistsInKVS == false && dataSeller.StagingData.ExistsInKVS == true {
+			deletedStaging := deleteDataCustomFromURL(prodSyncStgReadV2URL, dataSeller.SellerID)
 			dataResponse.OperationDetail = fmt.Sprintf("Eliminar %d en InStaging %s", dataSeller.SellerID, strconv.FormatBool(deletedStaging))
 		}
 
-		if dataSeller.InProd == false && dataSeller.InClon == true && dataSeller.InStaging == false {
-			deletedClon := deleteDataCustomFromURL(deleteClonURL, dataSeller.SellerID)
+		if dataSeller.ProdData.ExistsInKVS == false && dataSeller.ClonData.ExistsInKVS == true && dataSeller.StagingData.ExistsInKVS == false {
+			deletedClon := deleteDataCustomFromURL(prodSyncClonReadV2URL, dataSeller.SellerID)
 			dataResponse.OperationDetail = fmt.Sprintf("Eliminar %d en InClon %s", dataSeller.SellerID, strconv.FormatBool(deletedClon))
 		}
 
-		if dataSeller.InProd == false && dataSeller.InClon == true && dataSeller.InStaging == true {
-			deletedClon := deleteDataCustomFromURL(deleteClonURL, dataSeller.SellerID)
-			deletedStaging := deleteDataCustomFromURL(deleteStagingURL, dataSeller.SellerID)
+		if dataSeller.ProdData.ExistsInKVS == false && dataSeller.ClonData.ExistsInKVS == true && dataSeller.StagingData.ExistsInKVS == true {
+			deletedClon := deleteDataCustomFromURL(prodSyncClonReadV2URL, dataSeller.SellerID)
+			deletedStaging := deleteDataCustomFromURL(prodSyncStgReadV2URL, dataSeller.SellerID)
 			dataResponse.OperationDetail = fmt.Sprintf("Eliminar %d en InClon %s y InStaging %s", dataSeller.SellerID, strconv.FormatBool(deletedClon), strconv.FormatBool(deletedStaging))
 		}
 
-		if dataSeller.InProd == true && dataSeller.InClon == false && dataSeller.InStaging == false {
+		if dataSeller.ProdData.ExistsInKVS == true && dataSeller.ClonData.ExistsInKVS == false && dataSeller.StagingData.ExistsInKVS == false {
 			createdStaging := createCustomData(createStagingURL, dataSeller.SiteID, dataSeller.SellerID, "staging")
 			createdProd := createCustomData(createProdAndCloneURL, dataSeller.SiteID, dataSeller.SellerID, "prodAndClone")
 			dataResponse.OperationDetail = fmt.Sprintf("Se creó Prod %s and InStaging %s %d", strconv.FormatBool(createdProd), strconv.FormatBool(createdStaging), dataSeller.SellerID)
 		}
 
-		if dataSeller.InProd == true && dataSeller.InClon == false && dataSeller.InStaging == true {
+		if dataSeller.ProdData.ExistsInKVS == true && dataSeller.ClonData.ExistsInKVS == false && dataSeller.StagingData.ExistsInKVS == true {
 			dataResponse.OperationDetail = fmt.Sprintf("Crear en InClon %d", dataSeller.SellerID)
 		}
 
-		if dataSeller.InProd == true && dataSeller.InClon == true && dataSeller.InStaging == false {
+		if dataSeller.ProdData.ExistsInKVS == true && dataSeller.ClonData.ExistsInKVS == true && dataSeller.StagingData.ExistsInKVS == false {
 			created := createCustomData(createStagingURL, dataSeller.SiteID, dataSeller.SellerID, "staging")
 			dataResponse.OperationDetail = fmt.Sprintf("Se creó InStaging %d %s", dataSeller.SellerID, strconv.FormatBool(created))
+		}
+
+		if dataSeller.ProdData.ExistsInKVS == true && dataSeller.ClonData.ExistsInKVS == true && dataSeller.StagingData.ExistsInKVS == true {
+			dataResponse.DataAnalysis = &DataAnalysis{
+				PaymentMethods: PaymentMethodsNode{
+					Prod:    dataSeller.ProdData.PaymentMethods,
+					Clon:    dataSeller.ClonData.PaymentMethods,
+					Staging: dataSeller.StagingData.PaymentMethods,
+				},
+				Exclusions: ExclusionsNode{
+					Prod:    dataSeller.ProdData.Exclusions,
+					Clon:    dataSeller.ClonData.Exclusions,
+					Staging: dataSeller.StagingData.Exclusions,
+				},
+				Groups: GroupsNode{
+					Prod:    dataSeller.ProdData.Groups,
+					Clon:    dataSeller.ClonData.Groups,
+					Staging: dataSeller.StagingData.Groups,
+				},
+				AmountAllowed: AmountAllowedNode{
+					Prod:    dataSeller.ProdData.AmountAllowed,
+					Clon:    dataSeller.ClonData.AmountAllowed,
+					Staging: dataSeller.StagingData.AmountAllowed,
+				},
+				OwnPromosByUser: OwnPromosByUserNode{
+					Prod:    dataSeller.ProdData.OwnPromosByUser,
+					Clon:    dataSeller.ClonData.OwnPromosByUser,
+					Staging: dataSeller.StagingData.OwnPromosByUser,
+				},
+			}
 		}
 
 		dataResponseList = append(dataResponseList, dataResponse)
@@ -270,4 +380,58 @@ func deleteDataCustomFromURL(url string, seller int) bool {
 	}
 
 	return false
+}
+
+func ValidateLastUpdateIntoKVS(inputSellers InputSellers) []LastUpdatedKVS {
+	lastUpdatedKVS := make([]LastUpdatedKVS, 0)
+	for _, seller := range inputSellers.SellerIDs {
+		sellerData := LastUpdatedKVS{
+			SellerID:           seller,
+			ProdLastUpdated:    getOriginalDataCustomFromURL(prodSyncReadV2URL, seller).LastUpdated,
+			ClonLastUpdated:    getOriginalDataCustomFromURL(prodSyncClonReadV2URL, seller).LastUpdated,
+			StagingLastUpdated: getOriginalDataCustomFromURL(prodSyncStgReadV2URL, seller).LastUpdated,
+		}
+		lastUpdatedKVS = append(lastUpdatedKVS, sellerData)
+	}
+
+	return lastUpdatedKVS
+}
+
+func getOriginalDataCustomFromURL(url string, seller int) *OriginalDataKVS {
+	urlFull := fmt.Sprintf("%s%d", url, seller)
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", urlFull, nil)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	for key, value := range headers {
+		req.Header.Add(key, value)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	var originalDataKVS OriginalDataKVS
+
+	err = json.Unmarshal(body, &originalDataKVS)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return &originalDataKVS
 }
